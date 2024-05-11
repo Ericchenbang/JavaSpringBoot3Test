fe4e1710119659ff9dc5ca10c40ec4a062ac3902
Ericchenbang <ericchenbsng@gmail.com>
2024-05-11 23:41:23 +0800

    Totally complete module 3!

--- demo/src/main/java/com/example/demo/run/Run.java
+++ demo/src/main/java/com/example/demo/run/Run.java
@@ -1,5 +1,8 @@
 package com.example.demo.run;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
 import java.time.LocalDateTime;
 
 public record Run(
@@ -7,7 +10,18 @@ public record Run(
         String title,
         LocalDateTime startedOn,
         LocalDateTime completedOn,
+        @Positive
         Integer miles,
         Location location
 ) {
+    public Run{
+        if(!completedOn.isAfter(startedOn)){
+            throw new IllegalArgumentException("Completed On must be after Started On");
+        }
+        if (title.isEmpty()){
+            throw new IllegalArgumentException("Title must not be empty");
+        }
+    }
+
+
 }
--- demo/src/main/java/com/example/demo/run/RunController.java
+++ demo/src/main/java/com/example/demo/run/RunController.java
@@ -2,6 +2,7 @@ package com.example.demo.run;
 import java.util.List;
 import java.util.Optional;
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -29,12 +30,12 @@ public class RunController {
    }
    @ResponseStatus(HttpStatus.CREATED)
    @PostMapping("")
-   void create(@RequestBody Run run){
+   void create(@Valid @RequestBody Run run){
        runRepository.create(run);
    }
    @ResponseStatus(HttpStatus.NO_CONTENT)
    @PutMapping("/{id}")
-   void update(@RequestBody Run run, @PathVariable Integer id){
+   void update(@Valid @RequestBody Run run, @PathVariable Integer id){
        runRepository.update(run,id);
    }
     @ResponseStatus(HttpStatus.NO_CONTENT)
