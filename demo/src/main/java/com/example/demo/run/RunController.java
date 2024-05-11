176eeb555163b435ef86f586833a65b5aa326047
Ericchenbang <ericchenbsng@gmail.com>
2024-05-11 21:50:34 +0800

    Done with module 3 without the last part - variation

--- demo/src/main/java/com/example/demo/run/RunController.java
+++ demo/src/main/java/com/example/demo/run/RunController.java
@@ -1,9 +1,10 @@
 package com.example.demo.run;
 import java.util.List;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/runs")
@@ -20,6 +21,28 @@ public class RunController {
 
    @GetMapping("/{id}")
     Run findById(@PathVariable Integer id){
-       return runRepository.findById(id);
+       Optional<Run> run =  runRepository.findById(id);
+       if(run.isEmpty()){
+           throw new RunNotFoundException();
+       }
+       return run.get();
    }
+   @ResponseStatus(HttpStatus.CREATED)
+   @PostMapping("")
+   void create(@RequestBody Run run){
+       runRepository.create(run);
+   }
+   @ResponseStatus(HttpStatus.NO_CONTENT)
+   @PutMapping("/{id}")
+   void update(@RequestBody Run run, @PathVariable Integer id){
+       runRepository.update(run,id);
+   }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+   void delete(@PathVariable Integer id){
+       runRepository.delete(id);
+   }
+
+
+
 }
