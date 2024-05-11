7517ddafe98478e5a36e347917325252eef56b92
Ericchenbang <ericchenbsng@gmail.com>
2024-05-11 16:45:21 +0800

    Completed Module 2git status

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,16 @@
 package com.example.demo;
 
+import com.example.demo.run.Location;
+import com.example.demo.run.Run;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -12,7 +19,14 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		log.info("hhhhh");
+	}
+
+	@Bean
+	CommandLineRunner runner(){
+		return args -> {
+			Run run = new Run(1, "First Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 5, Location.OUTDOOR);
+			log.info("Run: " + run);
+		};
 	}
 
 }
