819888427b372d0010e5b22bb64ceb1c37268fd2
Ericchenbang <ericchenbsng@gmail.com>
2024-05-11 17:35:58 +0800

    Done with findById but without 99

---
+++ demo/src/main/java/com/example/demo/run/RunRepository.java
@@ -0,0 +1,43 @@
+package com.example.demo.run;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+@Repository
+public class RunRepository {
+    private List<Run> runs = new ArrayList<>();
+
+    public RunRepository() {
+        init();
+    }
+    List<Run> findAll(){
+        return runs;
+    }
+
+    Run findById(Integer id){
+        return runs.stream()
+                .filter(run -> run.id() == id)
+                .findFirst()
+                .get();
+    }
+
+    private void init(){
+        runs.add(new Run(1,
+                "Monday Morning Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plus(30,ChronoUnit.MINUTES),
+                3,
+                Location.INDOOR));
+
+        runs.add(new Run(2,
+                "Wednesday Evening Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plus(60,ChronoUnit.MINUTES),
+                6,
+                Location.OUTDOOR));
+    }
+
+}
