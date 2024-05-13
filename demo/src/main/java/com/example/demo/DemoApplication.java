fed2bb67e70ea123a3a59469ffe33fa3bb12d73a
Ericchenbang <ericchenbsng@gmail.com>
2024-05-14 00:30:18 +0800

    Completed module 5

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,20 @@
 package com.example.demo;
 
 
+import com.example.demo.user.User;
+import com.example.demo.user.UserHttpClient;
+import com.example.demo.user.UserRestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.util.List;
 
 
 @SpringBootApplication
@@ -16,12 +26,20 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-//	@Bean
-//	CommandLineRunner runner(RunRepository runRepository){
-//		return args -> {
-//			Run run = new Run(1, "First Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 5, Location.OUTDOOR);
-//			runRepository.create(run);
-//		};
-//	}
+	@Bean
+	UserHttpClient userHttpClient(){
+		RestClient restClient = RestClient.create("https://jsonplaceholder.typicode.com/");
+		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
+		return factory.createClient(UserHttpClient.class);
+
+	}
+
+	@Bean
+	CommandLineRunner runner(UserHttpClient client){
+		return args -> {
+				List<User> users = client.findAll();
+				System.out.println(users);
+		};
+	}
 
 }
